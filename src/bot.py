f287e47e66106873bcebc082674bb51286307dee
Georgii Beliaev <123897400+JoraXD@users.noreply.github.com>
2024-11-03 21:58:49 +0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,36 @@
 import asyncio
+import logging
+import datetime
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="6785842829:AAFHVBy_AOseywFfBx6uNxPdOhSHuL7frIA")
+# Диспетчер
+dp = Dispatcher()
 
-def startBot():
-    pass
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello! I am meeting creator bot, let me help you!")
 
+# Хээндлер на команду /new_meeting
+@dp.message(Command("new_meeting"))
+async def cmd_start(message: types.Message):
+    await message.answer("OK! Let me know at what date do You want to meet!\nWrite it in format DD-MM")
+    date_str = message.text
+    if IsDateValid(date_str):
 
+    try:
+        date_obj = datetime.strptime(date_str, "%d-%m-%Y")
+        await message.reply(date_obj.strftime("%d-%m-%Y"))
+    except :
+        await message.answer("Please enter a valid date in format DD-MM-YYYY")
+def IsDateAware(date):
+    return 0
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    startBot()
+    asyncio.run(main())
