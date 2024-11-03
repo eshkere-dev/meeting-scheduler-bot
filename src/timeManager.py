98c9c8704913d3680da8e3dfea327fccee065883
Georgii Beliaev <123897400+JoraXD@users.noreply.github.com>
2024-11-04 00:28:27 +0300

    Added main fuctions for bot, declared some fuctions in db and tm

---
+++ src/timeManager.py
@@ -0,0 +1,9 @@
+#Проверка правильности ввода даты
+def IsDateValid(data):
+    return 1
+#Проверка правильности ввода времени
+def IsTimeValid(time):
+    return 1
+#Проверка свободна ли дата,принимает в себя array[date,time]
+def IsDataAvailable(dateArray):
+    return 1
