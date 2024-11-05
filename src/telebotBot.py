cab25548babfc1682d1286c5ae5d16204aaa8c6e
Georgii Beliaev <123897400+JoraXD@users.noreply.github.com>
2024-11-06 01:36:49 +0300

    bugfix

--- src/telebotBot.py
+++ src/telebotBot.py
@@ -193,7 +193,7 @@ def meeting_deleter(message):
     if not(meeting_url.startswith("meet.jit.si/")):
         bot.send_message(message.chat.id, "Incorrect url provided")
 
-    if db.get_meeting_creator_id == message.chat.id and db.meeting_exist_by_url(meeting_url):
+    if db.get_meeting_creator_id == message.chat.id and db.meeting_url_exists(meeting_url):
         if db.delete_meeting_by_url(meeting_url):
             bot.send_message(message.chat.id, "Deleted succesfully")
         else:
